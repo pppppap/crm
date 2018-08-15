8c319b2250bf797507a31fdc04bba92966df7646
pppppap <1026516401@qq.com>
2018-08-15 14:27:49 +0800

    add html dir

--- org/src/main/java/cn/gezhi/crm/org/service/DepartmentService.java
+++ org/src/main/java/cn/gezhi/crm/org/service/DepartmentService.java
@@ -5,14 +5,23 @@ import cn.gezhi.crm.org.entity.*;
 import java.util.List;
 
 public interface DepartmentService {
- Department getOne(int id);
- int deleteOne(int id);
- int save(Department department);
- int update(Department department);
- int updateSelective(Department department);
- int saveSelective(Department department);
- List<Department> findSome(String keywords);
- List<Department> getAll(DepartmentExample example);
- PageModel<Department> getDepartmentPage(int page, int pageSize);
- PageModel<Department> getByExamplePage(int page, int pageSize, DepartmentExample example);
+    Department getOne(int id);
+
+    int deleteOne(int id);
+
+    int save(Department department);
+
+    int update(Department department);
+
+    int updateSelective(Department department);
+
+    int saveSelective(Department department);
+
+    List<Department> findSome(String keywords);
+
+    List<Department> getAll(DepartmentExample example);
+
+    PageModel<Department> getDepartmentPage(int page, int pageSize);
+
+    PageModel<Department> getByExamplePage(int page, int pageSize, DepartmentExample example);
 }
--- org/src/main/java/cn/gezhi/crm/org/service/impl/DepartmentServiceImpl.java
+++ org/src/main/java/cn/gezhi/crm/org/service/impl/DepartmentServiceImpl.java
@@ -21,83 +21,98 @@ import java.util.List;
 public class DepartmentServiceImpl implements DepartmentService {
     @Autowired
     private DepartmentMapper departmentMapper;
+
     /**
-     *TODO 根据ID查询一个部门
-     *@param id 部门的编号
-     *@return  部门对象
+     * 根据ID查询一个部门
+     *
+     * @param id 部门的编号
+     * @return 部门对象
      **/
 
     public Department getOne(int id) {
 
         return departmentMapper.selectByPrimaryKey(id);
     }
-/**
- *TODO 根据ID删除一个部门
- *@param id 部门的编号
- *@return  受影响的行数
- **/
+
+    /**
+     * 根据ID删除一个部门
+     *
+     * @param id 部门的编号
+     * @return 受影响的行数
+     **/
     public int deleteOne(int id) {
         return departmentMapper.deleteByPrimaryKey(id);
     }
-/**
- *TODO 新增一个部门
- *@param department 部门的一个对象
- *@return 受影响的行数
- **/
+
+    /**
+     * 新增一个部门
+     *
+     * @param department 部门的一个对象
+     * @return 受影响的行数
+     **/
     public int save(Department department) {
         return departmentMapper.insert(department);
     }
-/**
- *TODO  修改一个部门的信息,没有修改的信息默认为null;
- *@param department 部门的一个对象,用做装修改属性的容器.
- *@return 受影响的行数
- **/
+
+    /**
+     * 修改一个部门的信息,没有修改的信息默认为null;
+     *
+     * @param department 部门的一个对象,用做装修改属性的容器.
+     * @return 受影响的行数
+     **/
     public int update(Department department) {
         return departmentMapper.updateByPrimaryKey(department);
     }
 
     /**
-     *TODO 有选择性的修改部门的休息,没有修改的信息保持不变
-     *@param department 部门的一个对象,用做装修改属性的容器.
-     *@return  受影响的行数
+     * 有选择性的修改部门的休息,没有修改的信息保持不变
+     *
+     * @param department 部门的一个对象,用做装修改属性的容器.
+     * @return 受影响的行数
      **/
     public int updateSelective(Department department) {
         return departmentMapper.updateByPrimaryKeySelective(department);
     }
-/**
- *TODO 有选择性的增加部门信息,没有增加的为null
- *@param department 部门的一个对象
- *@return 受影响的行数
- **/
+
+    /**
+     * 有选择性的增加部门信息,没有增加的为null
+     *
+     * @param department 部门的一个对象
+     * @return 受影响的行数
+     **/
     public int saveSelective(Department department) {
         return departmentMapper.insertSelective(department);
     }
-/**
- *TODO 用生成的example做模糊查询
- *@param keywords 查询的关键字
- *@return  部门的集合
- **/
+
+    /**
+     * 用生成的example做模糊查询
+     *
+     * @param keywords 查询的关键字
+     * @return 部门的集合
+     **/
     public List<Department> findSome(String keywords) {
-        DepartmentExample example=new DepartmentExample();
+        DepartmentExample example = new DepartmentExample();
         DepartmentExample.Criteria criteria = example.createCriteria();
         criteria.andDepNameLike(keywords);
         return departmentMapper.selectByExample(example);
     }
-/**
- *TODO  查询所有的部门
- *@param example 全部列
- *@return  部门集合
- **/
+
+    /**
+     * 查询所有的部门
+     *
+     * @param example 全部列
+     * @return 部门集合
+     **/
     public List<Department> getAll(DepartmentExample example) {
         return departmentMapper.selectByExample(example);
     }
 
     public PageModel<Department> getDepartmentPage(int page, int pageSize) {
-        return getByExamplePage(page,pageSize,null);
+        return getByExamplePage(page, pageSize, null);
     }
 
     public PageModel<Department> getByExamplePage(int page, int pageSize, DepartmentExample example) {
-        PageHelper.startPage(page,pageSize);
+        PageHelper.startPage(page, pageSize);
         List<Department> departments = departmentMapper.selectByExample(example);
 
         return new PageModel<Department>(departments);
