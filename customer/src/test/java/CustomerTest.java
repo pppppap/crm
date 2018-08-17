8cbc6744bf14f89773151f03652bb4a96f8cc2ca
pppppap <pppap123@outlook.com>
2018-08-17 11:54:45 +0800

    添加customer模块

--- customer/src/test/java/CustomerTest.java
+++ customer/src/test/java/CustomerTest.java
@@ -1,7 +1,19 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * TODO
  *
  * @author pppppap
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-placeholder.xml", "classpath:spring/application*.xml"})
 public class CustomerTest {
+
+    @Test
+    public void test1() {
+    }
+
 }
