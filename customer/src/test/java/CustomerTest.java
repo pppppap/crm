7dcfb66035f03abc24c5188a5d642f4910c0f2de
pppppap <pppap123@outlook.com>
2018-08-23 16:39:18 +0800

    修改Customer展示页面大小

--- customer/src/test/java/CustomerTest.java
+++ customer/src/test/java/CustomerTest.java
@@ -1,10 +1,5 @@
-import cn.gezhi.crm.customer.dto.CustomerQueryDTO;
-import cn.gezhi.crm.customer.entity.vo.CustomerCustom;
-import cn.gezhi.crm.customer.service.EvaluateService;
-import cn.gezhi.crm.org.entity.PageModel;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,24 +11,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-placeholder.xml", "classpath:spring/application*.xml"})
 public class CustomerTest {
-    @Autowired
-    private EvaluateService evaluateService;
+
 
     @Test
     public void test1() {
-        CustomerQueryDTO queryDTO=new CustomerQueryDTO();
-        queryDTO.setProcess("已放款");
-        queryDTO.setAttentionLevel("B");
-        PageModel<CustomerCustom> customers = evaluateService.getCustomerPage(queryDTO);
-        for (CustomerCustom customerCustom : customers.getList()) {
 
-
-            System.out.println(customerCustom.getBank().getName());
-            System.out.println(customerCustom.getCars());
-            System.out.println(customerCustom.getEvaluteman().getName());
-            System.out.println(customerCustom.getHouses());
-            System.out.println(customerCustom.getSalesman().getName());
-        }
     }
 
 }
